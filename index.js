60960c19c3480f5cf61caf3ace4795d601306dd2
Bhavya Dua <dua_bhavya@outlook.com>
2022-11-02 17:02:38 -0400

    commit tree 839b2b84714f87e46ba936ad8dd077b8f350b857

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+        .then(resp => resp.json())
+        .then(data => appendId(data))
+        .catch(error => {
+            const errormsg = document.createElement('p');
+            errormsg.textContent = error.message;
+            document.querySelector('body').appendChild(errormsg);
+        })
+}
+
+function appendId(data) {
+    const userId = document.createElement('p');
+    userId.textContent = `${data.id}`;
+    document.querySelector('body').appendChild(userId);
+}
+
